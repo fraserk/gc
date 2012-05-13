0b0c098c4a25a174c2fc4c5a0cae274a10ac2917
saasbook <saasbook@saasbook.(none)>
2012-05-13 09:25:38 -0700

    Remove schedules from models

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,9 +6,8 @@ class Event < ActiveRecord::Base
   friendly_id :name, use: :slugged
   belongs_to :location
   belongs_to :user
-  has_many :schedules
   
   accepts_nested_attributes_for :location
-  accepts_nested_attributes_for :schedules, :allow_destroy => :true
+  
   
 end
