f814bb6069a0410f3377b025b8c9604a6ad92fc7
saasbook <saasbook@saasbook.(none)>
2012-05-13 09:51:56 -0700

    remove nested attri location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
 validates :location_name, :presence => true
 has_many :events
-accepts_nested_attributes_for :events, :allow_destroy => :true
+#accepts_nested_attributes_for :events, :allow_destroy => :true
 end
