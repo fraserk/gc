980ddba38df757b172509b5acc406910fd5daac7
fraserk <kimfraser@gmail.com>
2012-08-24 22:02:31 -0700

    Add 410 statud code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def render_404
+  if /(jpe?g|png|gif)/i === request.path
+    render :text => "404 Not Found", :status => 410 # Change the status here
+  else
+    render :file => "#{Rails.root}/public/404.html", :layout => 'application', :status => 410 #and here
+  end
+end
+
 end
